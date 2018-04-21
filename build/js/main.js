542c32f9b59608f8aaa26972ed0f8cb738196b7c
Skullyfox <Skullyfox.yt@gmail.com>
2018-04-21 19:43:15 +0200

    Feature Next and Previous + button size

--- build/js/main.js
+++ build/js/main.js
@@ -1,30 +1,60 @@
 "use strict"
 
-const   coverContent = document.querySelector('.albumPic'),
-        coverContentChild = coverContent.querySelectorAll('div'),
-        previous = document.querySelector('#previous'),
-        next = document.querySelector('#next');
+const coverContent = document.querySelector('.albumPic'),
+    coverContentChild = coverContent.querySelectorAll('div'),
+    previous = document.querySelector('#previous'),
+    next = document.querySelector('#next');
 
-let     count = 0;
+let currentTrackIndex = 0;
 
 /* Previous Button Click */
+
 previous.addEventListener('click', () => {
-    if(count > 0){
-        count--;
-    }else{
+
+    if (currentTrackIndex > 0) {
+
+        let previousCover = coverContentChild[currentTrackIndex - 1],
+            currentCover = coverContentChild[currentTrackIndex];
+
+        previousCover.classList.remove('hideleft');
+        previousCover.classList.add('showleft');
+        currentCover.classList.add('hideright');
+
+        --currentTrackIndex;
+
+    } else {
+
         console.log('not have music before');
     }
-    console.log(count)
-    return count;
+
+    console.log(currentTrackIndex)
+    return currentTrackIndex;
+
 });
 
-/* next button Click */
+/* Next button Click */
+
 next.addEventListener('click', () => {
-    if(count < coverContentChild.length){
-        count++
-    }else{
+
+    if (currentTrackIndex < coverContentChild.length - 1) {
+
+        let nextCover = coverContentChild[currentTrackIndex + 1],
+            currentCover = coverContentChild[currentTrackIndex];
+
+        nextCover.classList.remove('hideright');
+        nextCover.classList.add('showright');
+        currentCover.classList.add('hideleft');
+
+        ++currentTrackIndex;
+
+    } else {
+
         console.log('not have music after');
+
     }
-    console.log(count)
-    return count;
+
+    console.log(currentTrackIndex)
+
+    return currentTrackIndex;
+
 });
